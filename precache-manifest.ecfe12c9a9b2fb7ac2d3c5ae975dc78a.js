2e869c78247208dc69ea30295cb9fe29cb1d324a
Arnas <1arnasdickus1@gmail.com>
2019-05-06 17:18:56 +0300

    Updates

---
+++ precache-manifest.ecfe12c9a9b2fb7ac2d3c5ae975dc78a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37ad05cff2b61a93793b7a6663fe63f",
+    "url": "/kilo-image-search-web-app/index.html"
+  },
+  {
+    "revision": "2f94499ba2319c9b7165",
+    "url": "/kilo-image-search-web-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f3ecd093179502663a0b",
+    "url": "/kilo-image-search-web-app/static/css/main.10add2f2.chunk.css"
+  },
+  {
+    "revision": "2f94499ba2319c9b7165",
+    "url": "/kilo-image-search-web-app/static/js/2.55766631.chunk.js"
+  },
+  {
+    "revision": "f3ecd093179502663a0b",
+    "url": "/kilo-image-search-web-app/static/js/main.83f7c6b4.chunk.js"
+  },
+  {
+    "revision": "1ff8e1ba7bbe27ba1a41",
+    "url": "/kilo-image-search-web-app/static/js/runtime~main.a701fc6d.js"
+  }
+]);
